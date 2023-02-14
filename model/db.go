7f681cf842c0b91c1f6982786bc4b99d126ff1f3
Gabriel Esteves <brielesteves@gmail.com>
2023-02-14 12:07:18 -0300

    fix docker-compose

--- model/db.go
+++ model/db.go
@@ -53,5 +53,5 @@ func Init() {
 }
 
 func connToString(info connection) string {
-	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", info.User, info.Password, info.Host, info.Port, info.DBname)
+	return fmt.Sprintf("%s:%s@tcp(mysql)/%s?charset=utf8mb4&parseTime=True&loc=Local", info.User, info.Password, info.DBname)
 }
